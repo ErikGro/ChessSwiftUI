374aa77b1c67c61adcda0c4ab233bf870dad589b
Erik Grosskopf <erik.grosskopf@mail.de>
2022-03-03 12:01:04 +0100

    Initial Commit

---
+++ Shared/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Shared
+//
+//  Created by Privat on 03.03.22.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
